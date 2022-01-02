8f8e8d53401abe67fcfa6db56d83776a50f2d436
rmpolanco <ramonpolanco0205@gmail.com>
2022-01-02 02:13:33 -0500

    Updated my solution to Problem #19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -1,8 +1,21 @@
+const { truncateThreshold } = require("chai/lib/chai/config");
+
 class MySolution {
   constructor() {
     this.flag = false; // this is optional to use;
   }
+  binarySearch(nums,target) {
+    if(nums.length == 1 && nums[0]!=target)return false;
+    let mid = Math.floor(nums.length/2);
+    if(nums[mid]==target)return true;
+    else{
+      if(nums[mid]>target)return this.binarySearch(nums.slice(0,mid),target);
+      if(nums[mid]<target)return this.binarySearch(nums.slice(mid),target);
+    }
 
+  
+  }
+  /* Other solution using 4 parameters
   binarySearch(nums, target, left, right) {
     let l = left===undefined? 0: left;
     let r = right===undefined? nums.length-1:right;
@@ -15,6 +28,7 @@ class MySolution {
     }
     return false;
   }
+  */
 }
 
 // Do not edit this line;
